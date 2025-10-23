65ce1e79f6f8d4c92179c565fd836c5cfbea6976
SamHalvoe <samuelkrug98@gmail.com>
2025-10-23 18:49:45 +0200

    update ArduinoSQLiteEXTMEM.cpp

--- src/ArduinoSQLiteEXTMEM.cpp
+++ src/ArduinoSQLiteEXTMEM.cpp
@@ -1,10 +1,8 @@
 #include <Arduino.h> // for: Teensy extmem functions
 
-#include <smalloc.h> // for: size_t sm_szalloc(const void *); /* get size of allocation */
-
 #include "sqlite3.h" // for: SQLite related stuff
 
-/* This information may be useful:
+/* This information may be useful (from SQLite documentation):
 
 **** The default memory allocator ****
 
@@ -15,52 +13,68 @@ The default allocator implements memsize() by always allocating 8 extra bytes on
 
 */
 
+// Our allocator implements memsize() (=> sqlite3_extmem_size()) by always allocating 4 extra bytes
+// on each malloc() request and storing the size of the allocation in that 4-byte header.
+// => SizeHeaderType is the type for storing the size of the allocation, at the beginning of the allocation.
+using SizeHeaderType = uint32_t; // sizeof(uint32_t) == 4 bytes
+
+static inline void* excludeHeader(void* in_pointer)
+{
+  return static_cast<void*>(++static_cast<SizeHeaderType*>(in_pointer));
+}
+
+static inline void* includeHeader(void* in_pointer)
+{
+  return static_cast<void*>(--static_cast<SizeHeaderType*>(in_pointer));
+}
 
 // SQLite malloc wrapper for EXTMEM
-static void* sqlite3_extmem_malloc(int size)
+static void* sqlite3_extmem_malloc(int in_size)
 {
   Serial.println("sqlite3_extmem_malloc");
-  return extmem_malloc(size);
+  void* pointer = extmem_malloc(c_sizeHeaderSize + in_size);
+  *static_cast<SizeHeaderType*>(pointer) = static_cast<SizeHeaderType>(in_size);
+  return excludeHeader(pointer);
 }
 
 // SQLite free wrapper for EXTMEM
-static void sqlite3_extmem_free(void* ptr)
+static void sqlite3_extmem_free(void* in_pointer)
 {
   Serial.println("sqlite3_extmem_free");
-  extmem_free(ptr);
+  extmem_free(includeHeader(in_pointer));
 }
 
 // SQLite realloc wrapper for EXTMEM
-static void* sqlite3_extmem_realloc(void* ptr, int new_size)
+static void* sqlite3_extmem_realloc(void* in_pointer, int in_newSize)
 {
   Serial.println("sqlite3_extmem_realloc");
-  return extmem_realloc(ptr, new_size);
+  return extmem_realloc(includeHeader(in_pointer), in_newSize);
 }
 
 // Return the size of an allocation
-static int sqlite3_extmem_size(void* ptr)
+static int sqlite3_extmem_size(void* in_pointer)
 {
   Serial.println("sqlite3_extmem_size");
-  if (ptr == nullptr) { return 0; }
-  return static_cast<int>(sm_szalloc(ptr));
+  if (in_pointer == nullptr) { return 0; }
+  return static_cast<int>(*(--static_cast<SizeHeaderType*>(in_pointer)));
 }
 
 // Round up request size to allocation size
-static int sqlite3_extmem_roundup(int size)
+static int sqlite3_extmem_roundup(int in_size)
 {
   Serial.println("sqlite3_extmem_roundup");
-  return size;
+  return in_size;
 }
 
 // Initialize the memory allocator
-static int sqlite3_extmem_init(void* pAppData)
+static int sqlite3_extmem_init(void* in_appData)
 {
   // is already initialized by Teensy
   return SQLITE_OK;
 }
 
 // Shutdown the memory allocator
-static void sqlite3_extmem_shutdown(void* pAppData)
+static void sqlite3_extmem_shutdown(void* in_appData)
 {
   // Nothing to do - managed by Teensy
 }
@@ -74,7 +88,7 @@ static const sqlite3_mem_methods sqlite3_extmem_methods = {
     sqlite3_extmem_roundup,
     sqlite3_extmem_init,
     sqlite3_extmem_shutdown,
-    nullptr  // pAppData - not needed
+    nullptr  // in_appData - not needed
 };
 
 // Function to configure SQLite to use EXTMEM allocators
